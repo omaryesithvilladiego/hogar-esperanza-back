b03ce7dc463c9d6f3546b13b0db34a3143596648
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2024-08-17 07:37:28 -0300

    last commit

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(userRoute)
 
+app.use('/', (req,res) => {
+  res.send('site live')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
